45d21e0163236c414262378e167ca3929399039a
funfungu <>
2018-09-18 16:15:26 +0800

    init

---
+++ src/com/gu/Hello.java
@@ -0,0 +1,9 @@
+package com.gu;
+
+import com.google.gson.Gson;
+
+public class Hello {
+    public static void main(String[] args) {
+        Gson gson;
+    }
+}
---
+++ src/com/gu/HelloServlet.java
@@ -0,0 +1,16 @@
+package com.gu;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+    doGet(request,response );
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        PrintWriter writer = response.getWriter();
+        writer.print("hello servlet");
+
+    }
+}
